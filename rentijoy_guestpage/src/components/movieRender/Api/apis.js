72a97f8a095c1975720d5aed9638a843a2fc04d0
Pambali <91451814+Pambali@users.noreply.github.com>
2022-01-03 17:49:58 +0530

    Update apis.js

--- rentijoy_guestpage/src/components/movieRender/Api/apis.js
+++ rentijoy_guestpage/src/components/movieRender/Api/apis.js
@@ -7,5 +7,6 @@ export async function fetchMovieDetail(id) {
     return await axios.get(`http://localhost:4000/movie/findById?id=${id}`)
 }
 export async function fetchUserMovieDetail(id,mid) {
-    return await axios.get(`http://localhost:4000/user/movies/mymovies?id=${id}&movieid=${mid}`)
+    let fumd=await axios.get(`http://localhost:4000/user/movies/mymovies?id=${id}&movieid=${mid}`)
+    return fumd
 }
