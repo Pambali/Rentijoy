f3a9f53bcfef83cc706cb398e518acb3fbacbe4a
Pambali <91451814+Pambali@users.noreply.github.com>
2022-01-03 15:42:32 +0530

    Update usermoviesController.js

--- Node_sequalize/controllers/usermoviesController.js
+++ Node_sequalize/controllers/usermoviesController.js
@@ -4,25 +4,15 @@ const getUserMovies = async(req,res)=>{
     var id=query.id;
     var movieId=query.movieid;
     var movies=[];
-    //var movieslist=[]
+   
     try{
        const mymovies=await userMovies.findAll({
             where:{userId:id,MovieId:movieId},   
         }).then(res=>{
            movies=res;console.log(res);
         })
-        //  movies=JSON.stringify(movies)
-        //  console.log(movies)
-        // movies=JSON.parse(movies)
-        // console.log(movies)
-        // movies.map(async m=>{
-        // var  mymoviesList=await movie.findAll({
-        //     where:{MovieId:m.movieId},
-        //     include: [{model: movieCast,as:"movieCast"}]
-        // })
-        // movieslist.push(mymoviesList)
-    //})
-    if(movies){
+      
+       if(movies){
         res.status(200).json(movies[0]);}
     else {res.status(200).json({"BuyedStatus":false,"RentStatus":false})}
     }catch(e){
